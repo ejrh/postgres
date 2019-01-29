f395014170dad3d71756c80ff8343f54b9042410
ejrh <ejrh00@gmail.com>
2019-01-30 10:37:10 +1300

    Support range quals in Tid Scan
    This means queries with expressions such as "ctid >= ? AND ctid < ?" can be
    answered by scanning over that part of a table, rather than falling back to a
    full SeqScan.

--- src/backend/executor/execAmi.c
+++ src/backend/executor/execAmi.c
@@ -51,6 +51,7 @@
 #include "executor/nodeSubplan.h"
 #include "executor/nodeSubqueryscan.h"
 #include "executor/nodeTableFuncscan.h"
+#include "executor/nodeTidrangescan.h"
 #include "executor/nodeTidscan.h"
 #include "executor/nodeUnique.h"
 #include "executor/nodeValuesscan.h"
@@ -196,6 +197,10 @@ ExecReScan(PlanState *node)
 
 		case T_TidScanState:
 			ExecReScanTidScan((TidScanState *) node);
+			break;
+
+		case T_TidRangeScanState:
+			ExecReScanTidRangeScan((TidRangeScanState *) node);
 			break;
 
 		case T_SubqueryScanState:
@@ -524,6 +529,7 @@ ExecSupportsBackwardScan(Plan *node)
 
 		case T_SeqScan:
 		case T_TidScan:
+		case T_TidRangeScan:
 		case T_FunctionScan:
 		case T_ValuesScan:
 		case T_CteScan:
